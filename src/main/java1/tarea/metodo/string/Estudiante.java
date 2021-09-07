6c854674a03b2e0e3db5cfb3de6af24c5708de7b
Jhonny Ninabanda <jeninabandap@uce.edu.ec>
2021-09-07 16:21:02 -0500

    Tarea 26: 7/septiembre/2021

---
+++ src/main/java1/tarea/metodo/string/Estudiante.java
@@ -0,0 +1,22 @@
+package tarea.metodo.string;
+
+public class Estudiante {
+	
+	public String cedula;
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [cedula=" + cedula + "]";
+	}
+	
+
+
+}
